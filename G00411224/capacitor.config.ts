14b10c5d1f9e3d316595fb1eab8eb4366b7d369e
adamski-dev <adam.poplawski.programista@gmail.com>
2024-12-30 14:41:51 +0000

    Copy of Project into github from VM

---
+++ G00411224/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G00411224',
+  webDir: 'www'
+};
+
+export default config;
